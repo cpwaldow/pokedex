c84afbddca3dfa1a7714a85d61b72edc84e7d3f3
Carlos Eduardo <cpwaldow@gmail.com>
2022-10-14 23:30:44 -0300

    add loading component

--- src/components/Pokelist.jsx
+++ src/components/Pokelist.jsx
@@ -4,12 +4,15 @@ import PokeInfo from './PokeInfo';
 
 const Pokelist = () => {
   const [pokemon, setPokemon] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
   const fetchItens = async () => {
+    setLoading(true);
     const response = await fetch(
       'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0',
     );
     const data = await response.json();
-    return setPokemon(data.results);
+    setPokemon(data.results);
+    setLoading(false);
   };
 
   React.useEffect(() => {
@@ -17,6 +20,15 @@ const Pokelist = () => {
   }, []);
   return (
     <>
+      {loading && (
+        <section>
+          <img
+            src="https://media.giphy.com/media/yhfTY8JL1wIAE/giphy.gif"
+            alt="loading"
+          />
+          <p className="loading-card">Loading...</p>
+        </section>
+      )}
       <section className="container-list">
         {pokemon.map((element) => {
           return (
